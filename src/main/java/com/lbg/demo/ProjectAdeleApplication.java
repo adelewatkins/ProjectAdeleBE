8766cf12b87c20e31011574b4e77d4657825e149
adelewatkins <adele_watkins@hotmail.com>
2024-03-12 10:55:20 +0000

    init commit

---
+++ src/main/java/com/lbg/demo/ProjectAdeleApplication.java
@@ -0,0 +1,13 @@
+package com.lbg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAdeleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAdeleApplication.class, args);
+	}
+
+}
